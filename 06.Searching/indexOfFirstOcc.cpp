60578836b0333f2dc452698c67351b782e9edb6d
Adars108Yadav <adarsh108yadav@gmail.com>
2025-12-21 11:57:22 +0530

    New Ques

--- 06.Searching/indexOfFirstOcc.cpp
+++ 06.Searching/indexOfFirstOcc.cpp
@@ -1,7 +1,28 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main() {
-    
+int firstOcc(int arr[], int low, int high, int key)
+{
+    while (low <= high)
+    {
+        int mid = low + (high - low) / 2;
+        if (arr[mid] == key && (arr[mid] != arr[mid - 1] || mid == 0))
+            return mid;
+        else if (arr[mid] < key)
+            low = mid + 1;
+        else
+            high = mid - 1;
+    }
+    return -1;
+}
+
+int main()
+{
+    int arr[] = {1, 10, 10, 10, 20, 20, 40};
+    int low = 0;
+    int high = sizeof(arr) / sizeof(arr[0]);
+    int key;
+    cin >> key;
+    cout << firstOcc(arr, low, high, key);
     return 0;
 }
